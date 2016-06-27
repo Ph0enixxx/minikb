b718766859d5a06bd7c196591afb1a8fedee2c51
Ph0enixxx <754406613@qq.com>
2016-06-27 18:37:23 +0800

    Update test.py
    #
    #	邵英帅 6.27
    #	修复了行列错位的bug
    #	修复了同一格子中有两节课程不能同时读取的bug

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #
-#	邵英帅 6.14
-#
+#	邵英帅 6.27
+#	修复了行列错位的bug
+#	修复了同一格子中有两节课程不能同时读取的bug
 import xlrd
 import io 
 import re
@@ -10,7 +11,7 @@ import pymysql
 
 #怎么挑选课程？？？
 #1：2、3、4、5   2：10，11，12，13   3：19，20，21，22   4：27，28，29，30	5：36，37，38，39		6：44，45，46，47
-grades = [[2,3,4,5],[10,11,12,13],[19,20,21,22],[27,28,29,30],[36,37,38,39],[44,45,46,47]]
+grades = [[2,3,4,5,6,7,8,9],[10,11,12,13,14,15,16,17,18],[19,20,21,22,23,24,25,26],[27,28,29,30,31,32,33,34,35],[36,37,38,39,40,41,42,43],[44,45,46,47,48,49,50,51]]
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 conn=pymysql.connect(host='127.0.0.1',user='root',passwd='',db='Library',port=3306,charset='utf8')
 cur=conn.cursor()#获取一个游标
@@ -34,14 +35,22 @@ def getData(file):
 					#print(_str)
 					_str = re.sub(r"\(.*?\)","",_str)
 				#if _str != "":
+			_str = re.sub(r"^\n+$","",_str)
+
 			#print(_str)
 			#if l == j[-1]:
 			classes.append(_str)
 			#print("------------------\n")
 	return classes
-
+def trans(lst):
+	tmp = []
+	for i in range(0,6):
+		for j in range(0,7):
+			tmp.append(lst[j*6 + i])
+	return tmp
+# 1,7,13,19,25,31,36  2,8,14,20,26,32,37
 def makeSql(courseList = [],c_name = ""):
-	sql = "insert into wcourse (class_name,"
+	sql = "insert into course (class_name,"
 	for i in range(1,43):
 		sql += "s" + str(i) + ","
 	sql += "s43) values ('" + c_name + "',"
@@ -50,18 +59,24 @@ def makeSql(courseList = [],c_name = ""):
 	sql += "'')"
 	#print(sql)
 	return sql
-print(os.listdir('./xls/'))
-for i in os.listdir('./xls/'):
+#print(os.listdir('./xls/'))
+for i in os.listdir('./xls2/'):
 	#print(getData('./xls/中131-3.xls'))
 	print(i + "完成")
 	try:
-		cur.execute(makeSql(getData('./xls/' + i),i))
+		#print(trans(getData('./xls/' + i)))
+		cur.execute(makeSql(trans(getData('./xls2/' + i)),i[:-4]))
 		conn.commit()
 		print(i + "完成")
 	except:
+		#pass
 		print(i + "错误")
-	#print()
+#print(cur.execute(makeSql(trans(getData('./xls/中131-3.xls')),i)))
+#print(len(trans(getData('./xls/中131-3.xls'))))
 
+	#print()"""
+#ls = [x for x in range(0,47)]
+#print(trans(ls))
 #读取所有文件（测试先读取一个）  ok！！！
 #不要第一行
 #3456 11 12 13 14 21 22 23 24 ok
